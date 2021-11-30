67380740257fade78813555c4955cfc12918c7cd
jonisland <jogucareers@gmail.com>
2021-11-29 16:05:25 -0800

    added instagram

--- src/components/Body/Contacts.js
+++ src/components/Body/Contacts.js
@@ -1,37 +1,50 @@
 import classes from "./Contacts.module.css";
 
-import Github from "../../images/social_media/github.svg";
-import LinkedIn from "../../images/social_media/linkedin.svg";
-import Twitter from "../../images/social_media/twitter.svg";
+import GithubSVG from "../../images/social_media/github.svg";
+import LinkedInSVG from "../../images/social_media/linkedin.svg";
+import TwitterSVG from "../../images/social_media/twitter.svg";
+import InstagramSVG from "/Users/jonathanguerrero/Documents/GitHub/portfolio/my-app/src/images/social_media/Instagram/Instagram.svg";
 
 const Contacts = () => {
+  const info = [
+    {
+      name: "Github",
+      href: "https://github.com/jonisland/",
+      svg: GithubSVG,
+    },
+    {
+      name: "LinkedIn",
+      href: "https://www.linkedin.com/in/jogu/",
+      svg: LinkedInSVG,
+    },
+    {
+      name: "Twitter",
+      href: "https://twitter.com/jogutwit/",
+      svg: TwitterSVG,
+    },
+    {
+      name: "Instagram",
+      href: "https://www.instagram.com/joni_webdev/",
+      svg: InstagramSVG,
+    },
+  ];
+
   return (
     <div className={`${classes["contact"]} ${"container"}`} id="contact">
       <h1>Contact/Social</h1>
       <div class="text-align-center">
         <div class={classes["socialMedia"]}>
-          <figure>
-            <a href="https://github.com/jonisland" target="_blank">
-              <img src={Github} alt="github" />
-            </a>
-            <figcaption>Github</figcaption>
-          </figure>
-          <figure>
-            <a href="https://www.linkedin.com/in/jogu/" target="_blank">
-              <img src={LinkedIn} alt="linkedin" />
-            </a>
-            <figcaption>LinkedIn</figcaption>
-          </figure>
-          <figure>
-            <a href="https://twitter.com/jogutwit" target="_blank">
-              <img src={Twitter} alt="twitter" />
-            </a>
-            <figcaption>Twitter</figcaption>
-          </figure>
+          {info.map((social) => (
+            <figure>
+              <a href={social.href} target="_blank" rel="noreferrer">
+                <img src={social.svg} alt={social.name.toLowerCase()} />
+              </a>
+              <figcaption>{social.name}</figcaption>
+            </figure>
+          ))}
         </div>
         <a href="mailto:jogucareers@gmail.com">
           <h3 class={`${classes["email"]} margin-center`}>send me an email</h3>
-          {/* <h3 class="email margin-center">send me an email</h3> */}
         </a>
       </div>
     </div>
