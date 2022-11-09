e3aa8d7888b72729e909081157d1814c6d463a88
jonisland <jogucareers@gmail.com>
2022-11-09 15:35:08 -0800

    updated npm

--- src/components/Body/Contact/Contact.js
+++ src/components/Body/Contact/Contact.js
@@ -1,122 +1,5 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import classes from "./Contact.module.css";
-
-const Form = () => {
-  const [values, setValues] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    message: "",
-  });
-
-  const [submitted, setSubmitted] = useState(false);
-  const [valid, setValid] = useState(false);
-
-  const setFirstNameHandler = (e) => {
-    setValues({ ...values, firstName: e.target.value });
-  };
-
-  const setLastNameHandler = (e) => {
-    setValues({ ...values, lastName: e.target.value });
-  };
-
-  const setEmailHandler = (e) => {
-    setValues({ ...values, email: e.target.value });
-  };
-
-  const setMessageHandler = (e) => {
-    setValues({ ...values, message: e.target.value });
-  };
-
-  const submitFormHandler = (e) => {
-    e.preventDefault();
-    if (values.firstName && values.lastName && values.email && values.message) {
-      setValid(true);
-    }
-    setSubmitted(true);
-  };
-
-  return (
-    <div className={classes.container}>
-      <form onSubmit={submitFormHandler}>
-        {submitted && valid ? (
-          <div className={classes.success_message}>
-            Form has been submitted!
-          </div>
-        ) : null}
-
-        {submitted && !valid ? (
-          <div className={classes.failed_message}>Form was not submitted! </div>
-        ) : null}
-
-        <div className={classes.form_control}>
-          <input
-            id="name"
-            type="text"
-            name="fist-name"
-            onChange={setFirstNameHandler}
-            placeholder="First Name"
-            value={values.firstName}
-          />
-          {submitted && !values.firstName ? (
-            <span className={classes.input_error_message}>
-              please enter your name
-            </span>
-          ) : null}
-        </div>
-        <div className={classes.form_control}>
-          <input
-            id="name"
-            type="text"
-            name="last-name"
-            onChange={setLastNameHandler}
-            placeholder="Last Name"
-            value={values.lastName}
-          />
-          {submitted && valid && !values.lastName ? (
-            <span className={classes.input_error_message}>
-              please enter your last name
-            </span>
-          ) : null}
-        </div>
-        <div className={classes.form_control}>
-          <input
-            id="name"
-            type="email"
-            name="email"
-            onChange={setEmailHandler}
-            placeholder="Email"
-            value={values.email}
-          />
-          {submitted && valid && !values.email ? (
-            <span className={classes.input_error_message}>
-              please enter your email
-            </span>
-          ) : null}
-        </div>
-        <div className={classes.form_control}>
-          <textarea
-            id="name"
-            type="comment"
-            name="message"
-            onChange={setMessageHandler}
-            placeholder="Message"
-            value={values.message}
-            rows="5"
-          />
-          {submitted && valid && !values.message ? (
-            <span className={classes.input_error_message}>
-              please enter a comment
-            </span>
-          ) : null}
-        </div>
-        <div className="form-actions">
-          <button>Submit</button>
-        </div>
-      </form>
-    </div>
-  );
-};
 
 const Contact = () => {
   return (
@@ -129,14 +12,131 @@ const Contact = () => {
         <br /> to you as soon as possible!
         <br />
       </h4>
-      <div className={classes.adiv}>
-        <button>
-          <a href="mailto: jogucareers@gmail.com">Send Email</a>
-        </button>
-      </div>
-      {/* <Form /> */}
+      <iframe
+        src="https://www.cognitoforms.com/f/xWSnRERFcEud5UFUlnAXUQ/1"
+        style={{ border: 0, width: 700 }}
+        height="582"
+      ></iframe>
+      <script src="https://www.cognitoforms.com/f/iframe.js"></script>
     </div>
   );
 };
 
 export default Contact;
+
+// const Form = () => {
+//   const [values, setValues] = useState({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     message: "",
+//   });
+
+//   const [submitted, setSubmitted] = useState(false);
+//   const [valid, setValid] = useState(false);
+
+//   const setFirstNameHandler = (e) => {
+//     setValues({ ...values, firstName: e.target.value });
+//   };
+
+//   const setLastNameHandler = (e) => {
+//     setValues({ ...values, lastName: e.target.value });
+//   };
+
+//   const setEmailHandler = (e) => {
+//     setValues({ ...values, email: e.target.value });
+//   };
+
+//   const setMessageHandler = (e) => {
+//     setValues({ ...values, message: e.target.value });
+//   };
+
+//   const submitFormHandler = (e) => {
+//     e.preventDefault();
+//     if (values.firstName && values.lastName && values.email && values.message) {
+//       setValid(true);
+//     }
+//     setSubmitted(true);
+//   };
+
+//   return (
+//     <div className={classes.container}>
+//       <form onSubmit={submitFormHandler}>
+//         {submitted && valid ? (
+//           <div className={classes.success_message}>
+//             Form has been submitted!
+//           </div>
+//         ) : null}
+
+//         {submitted && !valid ? (
+//           <div className={classes.failed_message}>Form was not submitted! </div>
+//         ) : null}
+
+//         <div className={classes.form_control}>
+//           <input
+//             id="name"
+//             type="text"
+//             name="fist-name"
+//             onChange={setFirstNameHandler}
+//             placeholder="First Name"
+//             value={values.firstName}
+//           />
+//           {submitted && !values.firstName ? (
+//             <span className={classes.input_error_message}>
+//               please enter your name
+//             </span>
+//           ) : null}
+//         </div>
+//         <div className={classes.form_control}>
+//           <input
+//             id="name"
+//             type="text"
+//             name="last-name"
+//             onChange={setLastNameHandler}
+//             placeholder="Last Name"
+//             value={values.lastName}
+//           />
+//           {submitted && valid && !values.lastName ? (
+//             <span className={classes.input_error_message}>
+//               please enter your last name
+//             </span>
+//           ) : null}
+//         </div>
+//         <div className={classes.form_control}>
+//           <input
+//             id="name"
+//             type="email"
+//             name="email"
+//             onChange={setEmailHandler}
+//             placeholder="Email"
+//             value={values.email}
+//           />
+//           {submitted && valid && !values.email ? (
+//             <span className={classes.input_error_message}>
+//               please enter your email
+//             </span>
+//           ) : null}
+//         </div>
+//         <div className={classes.form_control}>
+//           <textarea
+//             id="name"
+//             type="comment"
+//             name="message"
+//             onChange={setMessageHandler}
+//             placeholder="Message"
+//             value={values.message}
+//             rows="5"
+//           />
+//           {submitted && valid && !values.message ? (
+//             <span className={classes.input_error_message}>
+//               please enter a comment
+//             </span>
+//           ) : null}
+//         </div>
+//         <div className="form-actions">
+//           <button>Submit</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
